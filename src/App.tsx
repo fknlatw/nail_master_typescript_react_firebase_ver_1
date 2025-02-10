467f271c6161eca45e2242cba0ac41d5f0d8a1b2
fknlatw <argat2480@gmail.com>
2025-02-10 17:47:46 +0600

    added logging_in

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,11 @@ import { AuthContext } from "./context/AuthContext";
 
 const App = () => {
   const {currentUser} = useContext(AuthContext)
-  // console.log(currentUser)
+  console.log(currentUser)
   const RequireAuth = ({children}: PropsWithChildren) => {
     return currentUser ? children : <Navigate to="/login" />
   }
+
   return (
     <div className="app">
       <Header />
