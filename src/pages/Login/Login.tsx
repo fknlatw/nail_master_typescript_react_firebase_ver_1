17cf9844b4c7a41b692b290075aec95fee6e3572
fknlatw <argat2480@gmail.com>
2025-02-10 17:22:57 +0600

    setting up firebase and login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,28 +1,64 @@
-
 import "./Login.scss";
+import {useContext, useState} from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from "../../firebase";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 const Login = () => {
+  const [error, setError] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   
+  const {dispatch} = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        dispatch({type: "LOGIN", payload: user});
+        navigate("/")
+      })
+      .catch((err) => {
+        const errorMessage = err.message;
+        setError(errorMessage);
+        return setTimeout(()=>{
+          setError("");
+        }, 3000)
+      })
+  }
 
   return (
     <div className="container login__container">
 
-      <form className="login__form">
+      <form  
+        onSubmit={handleLogin}
+        className="login__form"
+      >
         <h2>Вход в систему</h2>
-        <label htmlFor="userName">Имя пользователя</label>
+        <label htmlFor="userEmail">Почта</label>
 
         <input 
           type="text" 
-          name="userName"
+          onChange={e=>setEmail(e.target.value)}
+          name="userEmail"
+          value={email}
         />
         <label htmlFor="userPassword">Пароль</label>
 
         <input 
           type="text" 
           name="userPassword" 
+          onChange={e=>setPassword(e.target.value)}
+          value={password}
         />
 
         <button type="submit">Войти</button>
+        {error && <span className="login__error">{error}</span>}
       </form>
     </div>
   )
